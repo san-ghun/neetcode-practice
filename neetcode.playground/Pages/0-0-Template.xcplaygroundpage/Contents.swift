7cc970b7619ac78bc721fd514b6ceb73c838c820
Sanghun <po4865@gmail.com>
2022-11-28 21:40:49 +0100

    init: Create new pages and template

---
+++ neetcode.playground/Pages/0-0-Template.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+//: [Previous](@previous)
+
+import Foundation
+import XCTest
+
+// NeetCode - Blind 75
+// 0. Template (0 / 0)
+
+// MARK: - Practice
+/// Description
+func function(_ arg: [Int]) -> Bool {
+    return false
+}
+
+// MARK: - Test
+
+class SolutionTest: XCTestCase {
+    static var allTests = [
+        ("Test Example", testExample),
+    ]
+
+    func testExample() {
+//        XCTAssertEqual({}, _)
+//        XCTAssertEqual({}, _)
+//        XCTAssertEqual({}, _)
+    }
+}
+
+SolutionTest.defaultTestSuite.run()
+
+// MARK: - Solution
+
+//: [Next](@next)
